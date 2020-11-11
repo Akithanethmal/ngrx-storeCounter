ea79e127466c5e5e4066cd60a937098fa8c3267a
Akitha Nethmal <akithanethmal@gmail.com>
2020-11-11 21:50:20 +0530

    first commit

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -6,24 +6,24 @@ import { increment, decrement, reset } from '../counter.actions';
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css'],
 })
 export class MyCounterComponent {
   count$: Observable<number>;
 
   constructor(private store: Store<{ count: number }>) {
-    // TODO: This stream will connect to the current store `count` state
     this.count$ = store.select('count');
   }
 
   increment() {
-    // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
 
   reset() {
-    // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
 }
